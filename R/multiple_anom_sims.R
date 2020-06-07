1df19874dd2a99669e0518e67c41df02e3a65698
Martin Tveten <matveten@gmail.com>
2020-06-07 13:12:36 +0200

    Bug fix.

--- R/multiple_anom_sims.R
+++ R/multiple_anom_sims.R
@@ -124,7 +124,7 @@ multiple_anom_setup <- function(p = 10, precision_type = "banded",
                     "rho"         = rho,
                     "vartheta"    = vartheta,
                     "shape"       = shape)
-  tuning <- tuning_params(init_b = c(0.001, 0.1, 1, 2, 50), n_sim = 200)
+  tuning <- tuning_params(init_b = c(0.001, 0.1, 1, 2, 70), n_sim = 200)
   out_file <- "multiple_anom_FINAL.csv"
   list(variables = variables, data = data, method = method,
        tuning = tuning, out_file = out_file)
